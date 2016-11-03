28c5056c64cc74bd6b40907ad6650406b3dc0d8a
Andreas Lauser <and@poware.org>
2016-11-03 13:21:27 +0100

    add scripts jenkins for triggering a run of the Norne deck with the downstream flow and flow_ebos simulators
    I am pretty sceptical that this is that simple, but maybe there are
    nice surprises...

---
+++ jenkins/run-norne.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+pushd .
+cd deps/opm-data
+
+build_module_full opm-material
+
+# Run the norne case
+cd norne
+$WORKSPACE/$configuration/build-opm-simulators/bin/flow deck_filename=NORNE_ATW2013.DATA output_dir=OPM
+test $? -eq 0 || exit 1
+PATH=$WORKSPACE/$configuration/install/bin:$PATH ./plotwells.sh
+
+popd
