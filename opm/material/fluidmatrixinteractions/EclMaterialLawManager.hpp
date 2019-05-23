41b2b41be2b9727403d3c199f7d1a4f8fe18b896
hnil <Halvor.M.Nilsen@sintef.no>
2019-05-23 09:21:32 +0200

    Changes according to comments in pullrequest

--- opm/material/fluidmatrixinteractions/EclMaterialLawManager.hpp
+++ opm/material/fluidmatrixinteractions/EclMaterialLawManager.hpp
@@ -448,11 +448,11 @@ private:
             + (oilEnabled?1:0)
             + (waterEnabled?1:0);
 
-        if(numEnabled ==0){
+        if( numEnabled == 0 ){
             throw std::runtime_error("At least two fluid phases must be enabled. (Is: "+std::to_string(numEnabled)+")");
         }else if   (numEnabled == 1){
             threePhaseApproach_ = Opm::EclMultiplexerApproach::EclOnePhaseApproach;
-        } else if (numEnabled == 2) {
+        } else if ( numEnabled == 2) {
             threePhaseApproach_ = Opm::EclTwoPhaseApproach;
             if (!gasEnabled)
                 twoPhaseApproach_ = Opm::EclTwoPhaseOilWater;
